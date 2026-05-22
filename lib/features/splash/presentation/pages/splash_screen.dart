05810f71fe5c975b902a2e0ac3f87f136b0e6a10
Emmett <emmett@initialize.in>
2026-05-22 02:08:07 +0000

    Rollback splash boot animation changes

--- lib/features/splash/presentation/pages/splash_screen.dart
+++ lib/features/splash/presentation/pages/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -43,12 +42,6 @@ class _SplashScreenState extends State<SplashScreen>
     _pulseAnimation = Tween<double>(begin: 1.0, end: 1.08).animate(
       CurvedAnimation(parent: _pulseController, curve: Curves.easeInOut),
     );
-
-    if (kIsWeb) {
-      _entranceController.value = 1;
-      _pulseController.repeat(reverse: true);
-      return;
-    }
 
     _entranceController.forward().then((_) {
       _pulseController.repeat(reverse: true);
